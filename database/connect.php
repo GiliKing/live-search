ada775b6282707e5aa93da7201c975735cd2169a
Ogili Christian <chrisogili12@gmail.com>
2021-10-23 12:13:46 -0700

    second commit

--- database/connect.php
+++ database/connect.php
@@ -2,10 +2,10 @@
 
 // Establish a connection to the database
 
-$host = "localhost";
-$user = "root";
-$db_password = "";
-$db_name = "search"; 
+$host = "us-cdbr-east-04.cleardb.com";
+$user = "ba71e6c13b9089";
+$db_password = "136fabbc";
+$db_name = "heroku_722f5da6961d470"; 
 
 
 $conn = mysqli_connect($host, $user, $db_password, $db_name) or die ("could not connect");
