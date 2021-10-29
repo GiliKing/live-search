bf37616c1d1646504aedee38ed99e49009d1e3c2
Ogili Christian <chrisogili12@gmail.com>
2021-10-28 21:09:27 -0700

    third commit

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
 
 <?php
     session_start();
-    if(isset($_SESSION['users']['name']) && isset($_SESSION['users']['email'])) {
+    if(isset($_SESSION['users']['name']) && isset($_SESSION['users']['email']) && $_SESSION['user']['verified'] == 1) {
         header("location: user.php");
     } 
 ?>
